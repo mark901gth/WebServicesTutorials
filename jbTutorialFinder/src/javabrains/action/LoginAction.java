cfc91ec1298dfec6eff6534a579047946dfaa9be
Mark Levine <mlevine@preludesoftware.com>
2014-02-28 14:26:58 -0500

    after lesson 14

--- jbTutorialFinder/src/javabrains/action/LoginAction.java
+++ jbTutorialFinder/src/javabrains/action/LoginAction.java
@@ -1,8 +1,9 @@
 package javabrains.action;
 
-import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionSupport;
+import com.sun.xml.internal.ws.util.StringUtils;
 
-public class LoginAction implements Action
+public class LoginAction extends ActionSupport
 {
     private String userId;
     private String password;
@@ -30,7 +31,21 @@ public class LoginAction implements Action
         this.password = password;
     }
 
-
+    public void validate()
+    {
+        if ( userId.isEmpty() )
+        {
+            // UserId blank
+            addFieldError( "userId", "User ID cannot be blank" );
+        }
+        if ( password.isEmpty() )
+        {
+            // Password blank
+            addFieldError( "password", "Password cannot be blank" );
+        }
+    }
+    
+    
     public String execute()
     {
         if ( getUserId().equals( "userId" ) && getPassword().equals( "password" ) )
