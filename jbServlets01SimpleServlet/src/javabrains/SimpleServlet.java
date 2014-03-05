6bedfe6aa1f7dd22849a5c29299b51ffc0160cbb
Mark Levine <mlevine@preludesoftware.com>
2014-03-05 15:29:30 -0500

    added http session

--- jbServlets01SimpleServlet/src/javabrains/SimpleServlet.java
+++ jbServlets01SimpleServlet/src/javabrains/SimpleServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 // @formatter:off
 /**
@@ -25,8 +26,17 @@ public class SimpleServlet extends HttpServlet
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        System.out.println("Hello from GET method");
-        response.getWriter().print( "Hello from Servlet GET method" );
+        response.setContentType( "text/html" );
+        PrintWriter writer = response.getWriter();
+        String userName = request.getParameter( "name" );
+        
+        HttpSession session = request.getSession();            
+        if ( userName != "" && userName!= null )
+        {
+            session.setAttribute( "savedUserName", userName );
+        }
+        writer.println( "Request parameter has username as: " + userName );
+        writer.println( "Session parameter has username as: " + (String)session.getAttribute( "savedUserName" ) );
     }
 
 }
